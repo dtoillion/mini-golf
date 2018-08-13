d81500c1f44726e5cdc6f919de917f6861858e5e
Daniel Toillion <toillion@gmail.com>
2018-08-13 16:49:02 +0200

    project settings and utility assets

---
+++ Assets/_Scripts/GolfBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour {
+
+  private Rigidbody rb;
+  private Vector3 startPosition;
+  private Vector3 stopPosition;
+  private Vector3 aimDirection;
+
+	void Awake ()
+  {
+    rb = GetComponent<Rigidbody>();
+	}
+
+  void OnMouseDown ()
+  {
+    startPosition = Input.mousePosition;
+    Debug.Log(startPosition);
+  }
+
+  void OnMouseUp ()
+  {
+    stopPosition = Input.mousePosition;
+
+    aimDirection = startPosition - stopPosition;
+    aimDirection.Normalize();
+    rb.AddForce(aimDirection * 100f);
+
+    Debug.Log(aimDirection);
+  }
+
+}
